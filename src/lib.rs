f7b73f2025b7454cc6cbd3b51695442e068868ca
Andrew Toth <andrewstoth@gmail.com>
2022-12-18 22:30:38 -0500

    Updated to use clap

--- src/lib.rs
+++ src/lib.rs
@@ -1,112 +1,146 @@
-mod message_handler;
-
-use anyhow::Result;
-use bitcoin::consensus::Decodable;
-use bitcoin::hashes::hex::FromHex;
+use anyhow::{anyhow, Error, Result};
+use bitcoin::consensus::encode::CheckedData;
+use bitcoin::consensus::{serialize, Decodable};
 use bitcoin::network::address::Address;
 use bitcoin::network::constants::ServiceFlags;
-use bitcoin::network::message::RawNetworkMessage;
+use bitcoin::network::message::{CommandString, NetworkMessage, RawNetworkMessage, MAX_MSG_SIZE};
+use bitcoin::network::message_blockdata::Inventory;
+use bitcoin::network::message_compact_blocks::GetBlockTxn;
 use bitcoin::network::message_network::VersionMessage;
 use bitcoin::secp256k1::rand::Rng;
-use bitcoin::Network;
+use bitcoin::util::bip152::BlockTransactionsRequest;
 use bitcoin::{secp256k1, BlockHash};
-use log::{info, trace};
-use message_handler::{BroadcastState, MessageHandler};
+use log::trace;
 use std::io::{BufReader, Read, Write};
 use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpStream};
-use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use std::sync::mpsc::Sender;
+use std::time::{SystemTime, UNIX_EPOCH};
 
-/// Config options for sending
-pub struct Config {
-    /// The user agent for the initial version message
-    ///
-    /// Defaults to `"/Satoshi:23.0.0/"`
-    pub user_agent: String,
-    /// The block height for the initial version message
-    ///
-    /// Defaults to `749_000`
-    pub block_height: i32,
-    /// The network to use
-    ///
-    /// Defaults to [`Network::Bitcoin`]
-    pub network: Network,
-    /// The timeout duration for the initial connection to the node
-    ///
-    /// Default is 30 seconds but that might not be long enough for tor
-    pub connection_timeout: Duration,
-    /// The timeout duration for the handshake, sending inv, receiving getdata,
-    /// and finally sending the tx message
-    /// Note that if a node already has the tx then it will not respond with
-    /// getdata so a timeout here does not necessarily mean the node does not
-    /// have the tx
-    ///
-    /// Default is 30 seconds
-    pub send_tx_timeout: Duration,
-}
-
-impl Default for Config {
-    fn default() -> Self {
-        Config {
-            user_agent: String::from("/Satoshi:23.0.0/"),
-            block_height: 749_000,
-            network: Network::Bitcoin,
-            connection_timeout: Duration::from_secs(30),
-            send_tx_timeout: Duration::from_secs(30),
-        }
-    }
-}
-
-pub fn begin_requesting(address: SocketAddr, config: Option<Config>) -> Result<()> {
-    let config = config.unwrap_or_default();
-    let stream = TcpStream::connect(address)?;
-
-    info!("Connected to node at {:?}", address);
-
-    let version_message = build_version_message(&config, Some(address))?;
-
-    let mut message_handler = MessageHandler::new(
-        stream.try_clone()?,
-        config.network.magic(),
-        BlockHash::from_hex("0000000000000000000592a974b1b9f087cb77628bb4a097d5c2c11b3476a58e")?,
-    );
-    message_handler.send_version_msg(version_message)?;
-
-    let result = message_loop(stream.try_clone()?, &mut message_handler);
-
-    if let Ok(_) = result {
-        info!("Received block successfully");
-    }
-
-    trace!("Disconnecting");
-    // Ignore error on shutdown, since we might have already broadcasted successfully
-    let _ = stream.shutdown(std::net::Shutdown::Both);
-
-    result
-}
-
-fn message_loop<W: Write + Unpin, R: Read + Unpin + Send>(
-    read_stream: R,
-    message_handler: &mut MessageHandler<W>,
+pub fn request_witness_blocks(
+    stream: &mut TcpStream,
+    block_hash: BlockHash,
+    number: usize,
+    sender: &Sender<Option<Error>>,
+    magic: u32,
 ) -> Result<()> {
-    let mut reader = BufReader::with_capacity(4_000_000, read_stream);
-    loop {
-        let reply = RawNetworkMessage::consensus_decode(&mut reader)?;
-        message_handler.handle_message(reply.payload)?;
-        if message_handler.state() == BroadcastState::Done {
-            break;
-        }
-    }
+    perform_handshake(stream, magic)?;
+
+    let msg = RawNetworkMessage {
+        magic,
+        payload: NetworkMessage::GetData(vec![Inventory::WitnessBlock(block_hash)]),
+    };
+    make_requests(stream, msg, number)?;
+
+    receive_responses(stream, "block", sender)?;
+
     Ok(())
 }
 
-fn build_version_message(config: &Config, address: Option<SocketAddr>) -> Result<VersionMessage> {
+pub fn request_blocks(
+    stream: &mut TcpStream,
+    block_hash: BlockHash,
+    number: usize,
+    sender: &Sender<Option<Error>>,
+    magic: u32,
+) -> Result<()> {
+    perform_handshake(stream, magic)?;
+
+    let msg = RawNetworkMessage {
+        magic,
+        payload: NetworkMessage::GetData(vec![Inventory::Block(block_hash)]),
+    };
+    make_requests(stream, msg, number)?;
+
+    receive_responses(stream, "block", sender)?;
+
+    Ok(())
+}
+
+pub fn request_compact_blocks(
+    stream: &mut TcpStream,
+    block_hash: BlockHash,
+    number: usize,
+    sender: &Sender<Option<Error>>,
+    magic: u32,
+) -> Result<()> {
+    perform_handshake(stream, magic)?;
+
+    let msg = RawNetworkMessage {
+        magic,
+        payload: NetworkMessage::GetData(vec![Inventory::CompactBlock(block_hash)]),
+    };
+    make_requests(stream, msg, number)?;
+
+    receive_responses(stream, "cmpctblock", sender)?;
+
+    Ok(())
+}
+
+pub fn request_blocktxns(
+    stream: &mut TcpStream,
+    block_hash: BlockHash,
+    indexes: Vec<u64>,
+    number: usize,
+    sender: &Sender<Option<Error>>,
+    magic: u32,
+) -> Result<()> {
+    perform_handshake(stream, magic)?;
+
+    let msg = RawNetworkMessage {
+        magic,
+        payload: NetworkMessage::GetBlockTxn(GetBlockTxn {
+            txs_request: BlockTransactionsRequest {
+                block_hash,
+                indexes,
+            },
+        }),
+    };
+    make_requests(stream, msg, number)?;
+
+    receive_responses(stream, "blocktxn", sender)?;
+
+    Ok(())
+}
+
+fn perform_handshake(stream: &mut TcpStream, magic: u32) -> Result<()> {
+    let version_message = build_version_message()?;
+    let message = RawNetworkMessage {
+        magic,
+        payload: NetworkMessage::Version(version_message),
+    };
+    let _ = stream.write(&serialize(&message))?;
+    trace!("Sent version message");
+    let mut reader = BufReader::with_capacity(MAX_MSG_SIZE, stream.try_clone()?);
+    loop {
+        let reply = RawNetworkMessage::consensus_decode(&mut reader)?;
+        match reply.payload {
+            NetworkMessage::Version(_) => {
+                trace!("Received version message");
+                let message = RawNetworkMessage {
+                    magic,
+                    payload: NetworkMessage::Verack,
+                };
+                let _ = stream.write(&serialize(&message))?;
+                trace!("Sent verack message");
+            }
+            NetworkMessage::Verack => {
+                trace!("Received verack message");
+                break;
+            }
+            _ => {
+                trace!("Received message {:?}", reply.payload);
+            }
+        }
+    }
+    trace!("Handshake complete");
+    Ok(())
+}
+
+fn build_version_message() -> Result<VersionMessage> {
     let empty_address = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 0);
 
     let services = ServiceFlags::WITNESS;
-    let addr_recv = match address {
-        Some(addr) => Address::new(&addr, services),
-        None => Address::new(&empty_address, services),
-    };
+    let addr_recv = Address::new(&empty_address, services);
     let addr_from = Address::new(&empty_address, services);
     let nonce: u64 = secp256k1::rand::thread_rng().gen();
     let timestamp = SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs();
@@ -117,8 +151,44 @@ fn build_version_message(config: &Config, address: Option<SocketAddr>) -> Result
         addr_recv,
         addr_from,
         nonce,
-        config.user_agent.clone(),
-        config.block_height,
+        String::from("/BlockSpammer:1.0/"),
+        0,
     );
     Ok(msg)
+}
+
+fn make_requests<W: Write>(writer: &mut W, msg: RawNetworkMessage, number: usize) -> Result<()> {
+    let mut msgs = Vec::with_capacity(number);
+    for _ in 0..number {
+        msgs.push(serialize(&msg.clone()));
+    }
+    writer.write(&msgs.into_iter().flatten().collect::<Vec<_>>())?;
+
+    trace!("Sent {number} msgs");
+
+    Ok(())
+}
+
+fn receive_responses<R: Read>(
+    reader: R,
+    command: &str,
+    sender: &Sender<Option<Error>>,
+) -> Result<()> {
+    let mut reader = BufReader::with_capacity(MAX_MSG_SIZE, reader);
+
+    loop {
+        let _: u32 = Decodable::consensus_decode_from_finite_reader(&mut reader)?;
+        let cmd = CommandString::consensus_decode_from_finite_reader(&mut reader)?;
+        let _ = CheckedData::consensus_decode_from_finite_reader(&mut reader)?;
+        if cmd.to_string() == command {
+            trace!("Received {command} msg");
+            let Ok(_) = sender.send(None) else { break; };
+        } else if (command == "cmpctblock" || command == "blocktxn") && cmd.to_string() == "block" {
+            return Err(anyhow!("Received block response instead of expected {command}. Requested block is too deep in the chain. Try with a block that is <10 blocks deep from chain tip."));
+        }
+    }
+
+    trace!("Finished receiving");
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,136 @@
-use anyhow::Result;
-use spam_block_reqs::begin_requesting;
-use std::thread;
+use anyhow::{anyhow, Result};
+use bitcoin::{hashes::hex::FromHex, BlockHash, Network};
+use clap::Parser;
+use spam_block_reqs::{
+    request_blocks, request_blocktxns, request_compact_blocks, request_witness_blocks,
+};
+use std::{net::TcpStream, sync::mpsc::channel, thread, time::Instant};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Type of request to send
+    #[arg(short, long, value_enum, default_value_t = RequestType::WitnessBlock)]
+    request_type: RequestType,
+
+    /// Number of connections to create
+    #[arg(short, long, default_value_t = 4)]
+    connections: u8,
+
+    /// Number of requests to make
+    #[arg(short, long, default_value_t = 1000)]
+    number: usize,
+
+    /// Block hash to request
+    #[arg(
+        short,
+        long,
+        default_value_t = String::from("0000000000000000000592a974b1b9f087cb77628bb4a097d5c2c11b3476a58e")
+    )]
+    block_hash: String,
+
+    /// ip:port of bitcoind to connect to
+    #[arg(short, long, default_value_t = String::from("127.0.0.1:8333"))]
+    address: String,
+
+    /// Network to use (bitcoin, testnet, signet, regtest)
+    #[arg(short, long, default_value_t = String::from("bitcoin"))]
+    network: String,
+}
+
+#[derive(Debug, Clone, clap::ValueEnum)]
+enum RequestType {
+    WitnessBlock,
+    CompactBlock,
+    BlockTransactions,
+    LegacyBlock,
+}
 
 fn main() -> Result<()> {
     let _ = env_logger::builder()
         .target(env_logger::Target::Stdout)
         .try_init();
-    let mut handlers = Vec::new();
-    for _ in 0..8 {
-        let handler = thread::spawn(|| {
-            begin_requesting("127.0.0.1:8333".parse().unwrap(), None).unwrap();
+
+    let args = Args::parse();
+
+    let req = args.request_type;
+    let connections = args.connections as usize;
+    let number = args.number;
+    let block_hash = &args.block_hash;
+    let address = args.address;
+    let magic = match args.network.as_str() {
+        "bitcoin" => Network::Bitcoin.magic(),
+        "testnet" => Network::Testnet.magic(),
+        "signet" => Network::Signet.magic(),
+        "regtest" => Network::Regtest.magic(),
+        _ => {
+            return Err(anyhow!("Invalid network {}", args.network));
+        }
+    };
+
+    let number = number - number % connections;
+    let reqs_per_connection = number / connections;
+    let block_hash = BlockHash::from_hex(block_hash)?;
+
+    let (tx, rx) = channel();
+
+    for _ in 0..connections {
+        let tx_clone = tx.clone();
+        let req_clone = req.clone();
+        let address_clone = address.clone();
+        thread::spawn(move || {
+            let mut stream = match TcpStream::connect(address_clone) {
+                Err(e) => {
+                    let _ = tx_clone.send(Some(anyhow!("Could not connect: {e}")));
+                    return;
+                }
+                Ok(stream) => stream,
+            };
+            let res = match req_clone {
+                RequestType::WitnessBlock => request_witness_blocks(
+                    &mut stream,
+                    block_hash,
+                    reqs_per_connection,
+                    &tx_clone,
+                    magic,
+                ),
+                RequestType::CompactBlock => request_compact_blocks(
+                    &mut stream,
+                    block_hash,
+                    reqs_per_connection,
+                    &tx_clone,
+                    magic,
+                ),
+                RequestType::BlockTransactions => request_blocktxns(
+                    &mut stream,
+                    block_hash,
+                    vec![1],
+                    reqs_per_connection,
+                    &tx_clone,
+                    magic,
+                ),
+                RequestType::LegacyBlock => request_blocks(
+                    &mut stream,
+                    block_hash,
+                    reqs_per_connection,
+                    &tx_clone,
+                    magic,
+                ),
+            };
+            if res.is_err() {
+                let _ = tx_clone.send(res.err());
+            }
         });
-        handlers.push(handler)
     }
-    handlers
-        .into_iter()
-        .for_each(|handler| handler.join().unwrap());
+
+    let now = Instant::now();
+    for _ in 0..number {
+        if let Some(err) = rx.recv()? {
+            return Err(err);
+        }
+    }
+    let elapsed = now.elapsed();
+    println!("Received {number} responses in {:.2?}", elapsed);
 
     Ok(())
 }
